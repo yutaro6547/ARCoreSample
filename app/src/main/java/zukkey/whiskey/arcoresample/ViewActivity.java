cb909ad2b655334320b4831061c84a5c174d52a7
zukkey <y_vkwi_27_hunhun@yahoo.co.jp>
2018-08-22 18:23:36 +0900

    fix

--- app/src/main/java/zukkey/whiskey/arcoresample/ViewActivity.java
+++ app/src/main/java/zukkey/whiskey/arcoresample/ViewActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MotionEvent;
+import android.widget.Button;
 
 import com.google.ar.core.Anchor;
 import com.google.ar.core.HitResult;
@@ -15,9 +16,13 @@ import com.google.ar.sceneform.rendering.ViewRenderable;
 import com.google.ar.sceneform.ux.ArFragment;
 import com.google.ar.sceneform.ux.TransformableNode;
 
+import java.util.Random;
+
 public class ViewActivity extends AppCompatActivity {
   private ArFragment arFragment;
   private ViewRenderable viewRenderable;
+  private ViewRenderable textViewRenderable;
+  Integer count = 0;
 
   public static Intent createIntent(Context context) {
     return new Intent(context, ViewActivity.class);
@@ -34,6 +39,11 @@ public class ViewActivity extends AppCompatActivity {
         .build()
         .thenAccept(renderable -> viewRenderable = renderable);
 
+    ViewRenderable.builder()
+        .setView(this, R.layout.item_text)
+        .build()
+        .thenAccept(renderable -> textViewRenderable = renderable);
+
     arFragment.setOnTapArPlaneListener(
         (HitResult hitResult, Plane plane, MotionEvent motionEvent) -> {
           if (viewRenderable == null) {
@@ -44,14 +54,23 @@ public class ViewActivity extends AppCompatActivity {
           AnchorNode anchorNode = new AnchorNode(anchor);
           anchorNode.setParent(arFragment.getArSceneView().getScene());
 
-          TransformableNode view = new TransformableNode(arFragment.getTransformationSystem());
-          view.setParent(anchorNode);
-          view.setRenderable(viewRenderable);
-          view.getRotationController();
-          view.getScaleController();
-          view.getTranslationController();
-          view.select();
-
+          if (count == 0) {
+            TransformableNode text = new TransformableNode(arFragment.getTransformationSystem());
+            text.setParent(anchorNode);
+            text.setRenderable(textViewRenderable);
+            text.getRotationController();
+            text.getScaleController();
+            text.select();
+          } else {
+            TransformableNode view = new TransformableNode(arFragment.getTransformationSystem());
+            view.setParent(anchorNode);
+            view.setRenderable(viewRenderable);
+            view.getRotationController();
+            view.getScaleController();
+            view.getTranslationController();
+            view.select();
+          }
+          count += 1;
         });
   }
 }
